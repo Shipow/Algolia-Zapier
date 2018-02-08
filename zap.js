dfad4cd4fbd46d58aa4255fe3ae8f260f52acac4
Shipow <shipowlata@gmail.com>
2018-02-08 21:05:14 +0100

    update
    -you can nest properties in your record attribute name with a dot – ie “social.twitter”
    -format type are detected and pushed to Algolia (number, object/array, boolean, string)
    -any string containing null or undefined is removed.
    -there is a new search function to use the zap to locate a record in Algolia.

--- zap.js
+++ zap.js
@@ -1,27 +1,97 @@
 var Zap = {
-   update_pre_write: function(bundle) {
-
-       var myRecord = {};
-       var data = bundle.action_fields_full.record;
-
-       Object.keys(data).forEach(function(key,index) {
-          if (data[key]*1) {
-              data[key] = data[key]*1;
+  update_pre_write: function(bundle) {
+    var myRecord = {};
+    var data = bundle.action_fields_full.record;
+    var data = resolveJSONPaths(data);
+    var supported = ["object", "number", "boolean", "string"];
+    function tryType(o, f) {
+      switch (f) {
+        case "boolean":
+          var b = o.toLowerCase();
+          if (b !== "true" && b !== "false") {
+            throw "not a boolean";
           }
-          myRecord[key] = data[key];
-       });
+          return JSON.parse(b);
+        case "object":
+          if (o.match(/[^\d]\,/g)) {
+            var s = o.split(/\,(?! )/);
+          }
+          if (o.startsWith("{") || o.startsWith("[")) {
+            return JSON.parse(
+              o
+                .replace(/([a-zA-Z0-9]+?):([\"\'\s]+)/g, '"$1":$2')
+                .replace(/'/g, '"')
+            );
+          } else if (s.length > 1) {
+            return s;
+          } else {
+            throw "not an object";
+          }
+          break;
+        case "number":
+          var n = o.replace(/(\d)\,(\d)/g, "$1$2");
+          if (isNaN(n)) {
+            throw "not a number";
+          }
+          return n * 1;
+        case "string":
+          if (typeof o === undefined || o === null) break;
+          return o;
+      }
+    }
 
+    Object.keys(data).forEach(function(key, index) {
+      function upgradeFormat() {
+        for (var i = 0; i < supported.length; i++) {
+          try {
+            return tryType(data[key], supported[i]);
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      }
+      myRecord[key] = upgradeFormat();
+    });
 
+    function resolveJSONPaths(input) {
+      if (typeof input === "string") {
+        return input;
+      }
+      if (Array.isArray(input)) {
+        return input.map(resolveJSONPaths);
+      }
+      return Object.entries(input).reduce((acc, cur) => {
+        const [key, value] = cur;
+        if (key.includes(".")) {
+          const [firstPath, ...paths] = key.split(".");
+          if (acc[firstPath] === undefined) {
+            acc[firstPath] = {};
+          }
+          acc[firstPath] = $.extend(
+            true,
+            {},
+            acc[firstPath],
+            resolveJSONPaths({ [paths.join(".")]: value })
+          );
+        } else {
+          acc[key] = resolveJSONPaths(value);
+        }
+        return acc;
+      }, {});
+    }
+
+    //myRecord = JSON.stringify(resolveJSONPaths(myRecord));
     myRecord = JSON.stringify(myRecord);
 
-        return {
-          url: bundle.request.url,
-          method: bundle.request.method,
-          auth: bundle.request.auth,
-          headers: bundle.request.headers,
-          params: bundle.request.params,
-          data: myRecord
-
-        };
-    }
+    return {
+      url: bundle.request.url,
+      method: bundle.request.method,
+      auth: bundle.request.auth,
+      headers: bundle.request.headers,
+      params: bundle.request.params,
+      data: myRecord
+    };
+  }
 };
+
+module.exports = Zap;
---
+++ zap.test.js
@@ -0,0 +1,148 @@
+import Zap from "../zap";
+
+it("works", () => {
+  const tests = [
+    {
+      string: '["Cats","Dogs", "Snakes"]',
+      expected: ["Cats", "Dogs", "Snakes"],
+      type: "object"
+    },
+    {
+      string: "['Cats','Dogs','Snakes']",
+      expected: ["Cats", "Dogs", "Snakes"],
+      type: "object"
+    },
+    {
+      string: '{key1: "attribute1", key2: "attribute2"}',
+      expected: { key1: "attribute1", key2: "attribute2" },
+      type: "object"
+    },
+    {
+      string: '{format:"recgWimTyjTH3S2or", timestamp:  "2017-08-12T20:52:52"}',
+      expected: {
+        format: "recgWimTyjTH3S2or",
+        timestamp: "2017-08-12T20:52:52"
+      },
+      type: "object"
+    },
+    {
+      string: "Cats,Dogs,Snakes",
+      expected: ["Cats", "Dogs", "Snakes"],
+      type: "object"
+    },
+    {
+      string: "42",
+      expected: 42,
+      type: "number"
+    },
+    {
+      string: "0.42",
+      expected: 0.42,
+      type: "number"
+    },
+    {
+      string: "1,000",
+      expected: 1000,
+      type: "number"
+    },
+    {
+      string: "false",
+      expected: false,
+      type: "boolean"
+    },
+    {
+      string: "TRUE",
+      expected: true,
+      type: "boolean"
+    },
+    {
+      string: "string with a number and comma: 42,300",
+      expected: "string with a number and comma: 42,300",
+      type: "string"
+    },
+    {
+      string: "blabla",
+      expected: "blabla",
+      type: "string"
+    },
+    {
+      string: "rj45",
+      expected: "rj45",
+      type: "string"
+    },
+    {
+      string: "↑ capital raised is 74,220,000 ↑ employee count is 170",
+      expected: "↑ capital raised is 74,220,000 ↑ employee count is 170",
+      type: "string"
+    }
+  ];
+
+  const tests2 = [
+    {
+      input: {
+        attribute: {
+          some: "value1",
+          and: ["array", { one: "object" }, { "nested.test": "value3" }]
+        },
+        "attribute.child.child": "value2"
+      },
+      output: {
+        attribute: {
+          some: "value1",
+          and: ["array", { one: "object" }, { nested: { test: "value3" } }],
+          child: { child: "value2" }
+        }
+      }
+    },
+    {
+      input: {
+        attribute: {
+          one: {
+            two: "two"
+          }
+        },
+        "attribute.one.three": "three"
+      },
+      output: {
+        attribute: {
+          one: {
+            two: "two",
+            three: "three"
+          }
+        }
+      }
+    }
+  ];
+
+  var bundle = {
+    request: {
+      url: "",
+      method: "",
+      auth: "",
+      headers: "",
+      params: ""
+    },
+    action_fields_full: {
+      record: ""
+    }
+  };
+
+  tests.forEach(function(test) {
+    bundle.action_fields_full.record = { test: test.string };
+    expect(Zap.update_pre_write(bundle).data).toEqual(
+      '{"test":' + JSON.stringify(test.expected) + "}"
+    );
+    expect(typeof JSON.parse(Zap.update_pre_write(bundle).data).test).toEqual(
+      test.type
+    );
+  });
+
+  tests2.forEach(function(test) {
+    bundle.action_fields_full.record = test.input;
+    expect(Zap.update_pre_write(bundle).data).toEqual(
+      JSON.stringify(test.output)
+    );
+  });
+
+  console.log("√ All test passed");
+});
